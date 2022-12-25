317df2f9b27f7e913c2df2751cf83739c5dff095
Nur Diarto <nur.diarto@taspen.co.id>
2022-12-25 11:35:33 +0700

    carousell

--- resources/views/frontend/home.blade.php
+++ resources/views/frontend/home.blade.php
@@ -4,36 +4,39 @@
     <!-- ======= Hero Section ======= -->
     <!-- <section id="hero" class="d-flex align-items-center"> -->
 
-    <div id="carouselExampleCaptions" class="carousel slide" data-bs-ride="carousel">
+    <div id="carouselHome" class="carousel slide" data-bs-ride="carousel">
         <div class="carousel-indicators">
-            <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
-            <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1" aria-label="Slide 2"></button>
-            <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2" aria-label="Slide 3"></button>
+            <button type="button" data-bs-target="#carouselHome" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+            <button type="button" data-bs-target="#carouselHome" data-bs-slide-to="1" aria-label="Slide 2"></button>
+            <button type="button" data-bs-target="#carouselHome" data-bs-slide-to="2" aria-label="Slide 3"></button>
         </div>
         <div class="carousel-inner">
-            <div class="carousel-item active" style="background-image: url('https://source.unsplash.com/LAaSoL0LrYs/1920x1080')">
+            <div class="carousel-item active" style="background-image: url('{{asset('assets/img/ocean.jpg')}}')">
             <div class="carousel-caption">
-                <!-- <h5>First slide label</h5>
-                <p>Some representative placeholder content for the first slide.</p> -->
+                <h1>Solusi Interaksi Ekonomi Biru</h1>
+                <h2>Platform digital untuk mendukung koordinasi dan sinergi stakeholder ekonomi biru</h2>
+                <a href="{{url('/admin')}}}" class="btn-get-started scrollto">Bergabung bersama kami</a>
             </div>
             </div>
-            <div class="carousel-item" style="background-image: url('https://source.unsplash.com/bF2vsubyHcQ/1920x1080')">
+            <div class="carousel-item" style="background-image: url('{{asset('assets/img/beach1.jpeg')}}')">
             <div class="carousel-caption">
-                <!-- <h5>Second slide label</h5>
-                <p>Some representative placeholder content for the second slide.</p> -->
+                <h1>Solusi Interaksi Ekonomi Biru</h1>
+                <h2>Platform digital untuk mendukung koordinasi dan sinergi stakeholder ekonomi biru</h2>
+                <a href="{{url('/admin')}}}" class="btn-get-started scrollto">Bergabung bersama kami</a>
             </div>
             </div>
-            <div class="carousel-item" style="background-image: url('https://source.unsplash.com/szFUQoyvrxM/1920x1080')">
+            <div class="carousel-item" style="background-image: url('{{asset('assets/img/beach2.jpeg')}}')">
             <div class="carousel-caption">
-                <!-- <h5>Third slide label</h5>
-                <p>Some representative placeholder content for the third slide.</p> -->
+                <h1>Solusi Interaksi Ekonomi Biru</h1>
+                <h2>Platform digital untuk mendukung koordinasi dan sinergi stakeholder ekonomi biru</h2>
+                <a href="{{url('/admin')}}}" class="btn-get-started scrollto">Bergabung bersama kami</a>
             </div>
             </div>
         </div>
-        <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="prev">
+        <button class="carousel-control-prev" type="button" data-bs-target="#carouselHome" data-bs-slide="prev">
             <span class="carousel-control-prev-icon" aria-hidden="true"></span>
         </button>
-        <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="next">
+        <button class="carousel-control-next" type="button" data-bs-target="#carouselHome" data-bs-slide="next">
             <span class="carousel-control-next-icon" aria-hidden="true"></span>
         </button>
     </div>
