1a6f11ba30003e75a286272b3d86a17441baabfb
Nur Diarto <nur.diarto@taspen.co.id>
2022-12-25 11:22:00 +0700

    carousell

--- resources/views/frontend/home.blade.php
+++ resources/views/frontend/home.blade.php
@@ -2,9 +2,42 @@
 
 @section('content')
     <!-- ======= Hero Section ======= -->
-    <section id="hero" class="d-flex align-items-center">
+    <!-- <section id="hero" class="d-flex align-items-center"> -->
 
-        <div class="container" data-aos="zoom-out" data-aos-delay="100">
+    <div id="carouselExampleCaptions" class="carousel slide" data-bs-ride="carousel">
+        <div class="carousel-indicators">
+            <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+            <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1" aria-label="Slide 2"></button>
+            <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2" aria-label="Slide 3"></button>
+        </div>
+        <div class="carousel-inner">
+            <div class="carousel-item active" style="background-image: url('https://source.unsplash.com/LAaSoL0LrYs/1920x1080')">
+            <div class="carousel-caption">
+                <!-- <h5>First slide label</h5>
+                <p>Some representative placeholder content for the first slide.</p> -->
+            </div>
+            </div>
+            <div class="carousel-item" style="background-image: url('https://source.unsplash.com/bF2vsubyHcQ/1920x1080')">
+            <div class="carousel-caption">
+                <!-- <h5>Second slide label</h5>
+                <p>Some representative placeholder content for the second slide.</p> -->
+            </div>
+            </div>
+            <div class="carousel-item" style="background-image: url('https://source.unsplash.com/szFUQoyvrxM/1920x1080')">
+            <div class="carousel-caption">
+                <!-- <h5>Third slide label</h5>
+                <p>Some representative placeholder content for the third slide.</p> -->
+            </div>
+            </div>
+        </div>
+        <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="prev">
+            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+        </button>
+        <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="next">
+            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+        </button>
+    </div>
+        <!-- <div class="container" data-aos="zoom-out" data-aos-delay="100">
             <div class="row">
                 <div class="col-xl-6">
                     <h1>Solusi Interaksi Ekonomi Biru</h1>
@@ -13,11 +46,9 @@
                 </div>
             </div>
         </div>
-      </div>
-
-    </div>
-  </section><!-- End About Section -->
-
+      </div> -->
+  <!-- </section> -->
+  <!-- End About Section -->
   <!-- ======= Clients Section ======= -->
   <section id="locations" class="locations">
     <div class="container" data-aos="zoom-in">
@@ -63,6 +94,9 @@
                       </ul>
                     </div>
                 </div>
+            </div>
+            @endforeach
+        @endif
     </section>
 
     <!-- ======= Clients Section ======= -->
