384329d18f799998a088b23e814849546a6b4970
Nur Diarto <nur.diarto@taspen.co.id>
2023-01-08 08:59:21 +0700

    slider, resources, map

--- app/Http/Controllers/Frontend/HomeController.php
+++ app/Http/Controllers/Frontend/HomeController.php
@@ -6,6 +6,7 @@ use App\Models\ResourceLibrary;
 use App\Models\ContentCategory;
 use App\Models\ContentPage;
 use App\Models\Event;
+use App\Models\BannerSlider;
 
 class HomeController
 {
@@ -21,11 +22,14 @@ class HomeController
             ->orderBy('created_at', 'DESC')
             ->get();
 
-        $resources = ResourceLibrary::all();
+        $resources = ResourceLibrary::where('type', 1)->get();
         // dd($resource);
+        $sliders = BannerSlider::where('publish_status', config('constant.general_publish_status.published'))
+        ->orderBy('created_at', 'DESC')
+        ->get();
         $contentCategories = ContentCategory::get()->whereNotIn('slug', ['about', 'regulation']);
 
-        return view('frontend.home', compact('events', 'articles', 'contentCategories', 'resources'));
+        return view('frontend.home', compact('events', 'articles', 'sliders', 'contentCategories', 'resources'));
     }
 
     public function test()
